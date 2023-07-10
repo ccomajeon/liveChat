b4727c5b373a6c2718d2009fce49bf5436ab4dc6
jeon jun young <jww565274@gmail.com>
2023-07-10 16:55:57 +0900

    setting

---
+++ demo/src/main/java/com/liveChat/board/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.liveChat.board.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(HttpServletRequest request) {
+		return "index";
+	}
+
+}
